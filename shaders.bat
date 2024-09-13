0d7eaf9209976d3a6c4aab543d8261c052f05fd7
Sebastian Strobl <sebastian@strobl.net>
2024-09-13 17:19:09 +0200

    Add: vert and frag shader
    Signed-off-by: Sebastian Strobl <sebastian@strobl.net>

---
+++ shaders.bat
@@ -0,0 +1,4 @@
+glslc assets/gradient.comp -o assets/gradient.comp.spv
+glslc assets/gradient_color.comp -o assets/gradient_color.comp.spv
+glslc assets/colored_triangle.vert -o assets/colored_triangle.vert.spv
+glslc assets/colored_triangle.frag -o assets/colored_triangle.frag.spv
